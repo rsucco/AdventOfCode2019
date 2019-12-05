782dbb8863bccb17098725e1dbd1251c460cb5f4
Ryan Succo <rsucco@live.com>
2019-12-04 23:33:56 -0500

    Day 1 Part 1 finished.

---
+++ Day1/Day1Part1.py
@@ -0,0 +1,11 @@
+
+totalFuel = 0
+inputData = []
+with open('./Day1Part1Data.txt', 'r') as inputFile:
+    for inputDatum in inputFile:
+        inputData.append(inputDatum.replace('\n', ''))
+
+for moduleMass in inputData:
+    totalFuel += int(moduleMass) // 3 - 2
+
+print (totalFuel)
